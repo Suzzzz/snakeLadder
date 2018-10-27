ea0e82d8206dd5c59ea7fc17d792d05630018dcd
Suzzzz <suhamendis@gmail.com>
2018-10-27 22:27:23 +0530

    Add files via upload

---
+++ SnakeNLadderTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.suharsha;
+
+/**
+ *
+ * @author suzzzz
+ */
+public class SnakeNLadderTest {
+    
+    public static void main(String[] args) {
+        SnakeNLadder s = new SnakeNLadder();
+        s.startGame();
+ 
+    }
+}
